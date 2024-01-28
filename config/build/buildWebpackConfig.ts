4823b7bff6a57e945789efc492f6ae47180b1a03
bogdan <bogdankuzzmin@gmail.com>
2024-01-28 20:46:14 +0500

    add env variables, build prod/dev

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -3,10 +3,11 @@ import { BuildOptions } from './types/config';
 import { buildPlugins } from './buildPlugins';
 import { buildLoaders } from './buildLoaders';
 import { buildResolvers } from './buildResolvers';
-import {buildDevServer} from './buildDevServer'
+import { buildDevServer } from './buildDevServer';
 
 export const buildWebpackConfig = (options: BuildOptions): Configuration => {
-  const { paths, mode } = options;
+  const { paths, mode, isDev } = options;
+
   return {
     mode,
     entry: paths.entry,
@@ -20,7 +21,7 @@ export const buildWebpackConfig = (options: BuildOptions): Configuration => {
       rules: buildLoaders(),
     },
     resolve: buildResolvers(),
-    devtool: 'inline-source-map',
-    devServer: buildDevServer(options),
-  }
+    devtool: isDev ? 'inline-source-map': undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
+  };
 };
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,23 +1,22 @@
 import path from 'path';
-import { Configuration } from 'webpack';
-import { buildWebpackConfig } from './config/build/buildWebpackConfig';
-import { BuildPaths } from './config/build/types/config';
+import {buildWebpackConfig} from './config/build/buildWebpackConfig';
+import {BuildEnv, BuildPaths} from './config/build/types/config';
 
-const paths: BuildPaths = {
-  entry: path.resolve(__dirname, 'src', 'index.ts'),
-  build: path.resolve(__dirname, 'build'),
-  html: path.resolve(__dirname, 'public', 'index.html'),
+export default (env: BuildEnv) => {
+  const paths: BuildPaths = {
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    build: path.resolve(__dirname, 'build'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
+  };
+
+  const mode = env.mode || 'development';
+  const PORT = env.port || 3000;
+  const isDev = mode === 'development';
+
+  return buildWebpackConfig({
+    mode,
+    paths,
+    isDev,
+    port: PORT,
+  });
 };
-
-const mode = 'development';
-const isDev = mode === 'development';
-const PORT = 3000;
-
-const config: Configuration = buildWebpackConfig({
-  mode,
-  paths,
-  isDev,
-  port: PORT,
-});
-
-export default config;
