cedae555e41c9fbeb660b524f8934ba1f6c70bd1
bogdan <bogdankuzzmin@gmail.com>
2024-01-30 01:10:27 +0500

    add font variables

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import MainPage from '@pages/Main';
-import '@styles/global.scss';
+import '@styles/index.scss';
 
 const root = createRoot(document.getElementById('root'));
 
