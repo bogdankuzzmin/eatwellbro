324708deb07712dcaca9e724b8d5f257a9d8a61d
bogdan <bogdankuzzmin@gmail.com>
2024-01-30 00:56:58 +0500

    rename theme to styles

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -4,7 +4,7 @@ import { BuildOptions } from './types/config';
 export const buildResolvers = ({paths}: BuildOptions): ResolveOptions => ({
   alias: {
     '@pages': `${paths.src}/pages`,
-    '@theme': `${paths.src}/theme`,
+    '@styles': `${paths.src}/styles`,
     '@utils': `${paths.src}/utils`,
   },
   extensions: ['.tsx', '.ts', '.js'],
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import MainPage from '@pages/Main';
-import '@theme/global.scss';
+import '@styles/global.scss';
 
 const root = createRoot(document.getElementById('root'));
 
