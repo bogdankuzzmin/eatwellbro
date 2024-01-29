3f52c0f290875d2f838c5d6a545326abce2d5d33
bogdan <bogdankuzzmin@gmail.com>
2024-01-30 02:46:39 +0500

    add contexts, hooks in aliases

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -3,6 +3,8 @@ import { BuildOptions } from './types/config';
 
 export const buildResolvers = ({paths}: BuildOptions): ResolveOptions => ({
   alias: {
+    '@contexts': `${paths.src}/contexts`,
+    '@hooks': `${paths.src}/hooks`,
     '@pages': `${paths.src}/pages`,
     '@styles': `${paths.src}/styles`,
     '@utils': `${paths.src}/utils`,
