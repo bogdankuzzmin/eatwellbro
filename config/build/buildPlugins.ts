1273aff3d73ab75bd6e68b792ab663feb1ab6146
bogdan <bogdankuzzmin@gmail.com>
2024-01-29 21:51:13 +0500

    install react, configure css modules

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,10 +1,15 @@
 import { WebpackPluginInstance, ProgressPlugin } from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { BuildOptions } from './types/config';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 export const buildPlugins = ({ paths }: BuildOptions): WebpackPluginInstance[] => ([
   new HtmlWebpackPlugin({
     template: paths.html,
   }),
   new ProgressPlugin(),
+  new MiniCssExtractPlugin({
+    filename: 'css/[name].[contenthash:8].css',
+    chunkFilename: 'css/[name].[contenthash:8].css',
+  }),
 ]);
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,10 +1,10 @@
 import path from 'path';
-import {buildWebpackConfig} from './config/build/buildWebpackConfig';
-import {BuildEnv, BuildPaths} from './config/build/types/config';
+import { buildWebpackConfig } from './config/build/buildWebpackConfig';
+import { BuildEnv, BuildPaths } from './config/build/types/config';
 
 export default (env: BuildEnv) => {
   const paths: BuildPaths = {
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
     build: path.resolve(__dirname, 'build'),
     html: path.resolve(__dirname, 'public', 'index.html'),
   };
