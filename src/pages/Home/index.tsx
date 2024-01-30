750e1703c41029e42a142e67b43c63f356612e99
bogdan <bogdankuzzmin@gmail.com>
2024-01-30 15:17:10 +0500

    add classNames util function, add className app to Main Page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,16 @@
 import useTheme from '@hooks/useTheme';
-import '@styles/index.scss';
 
 const HomePage = () => {
-  const {theme, toggleTheme} = useTheme();
+  const {toggleTheme} = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <>
       <h2>
         Home page
       </h2>
 
       <button onClick={toggleTheme}>Change theme</button>
-    </div>
+    </>
   );
 };
 
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,19 +1,26 @@
 import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import { Paths } from '@utils/constants/routes';
-
 const HomePage = lazy(() => import('../Home'));
 const NotFoundPage = lazy(() => import('../NotFound'));
 
+import useTheme from '@hooks/useTheme';
+import { Paths } from '@utils/constants/routes';
+import classNames from '@utils/classNames';
+import '@styles/index.scss';
+
 const MainPage = () => {
+  const {theme} = useTheme();
+
   return (
-    <Suspense>
-      <Routes>
-        <Route path={Paths.root} element={<HomePage />}/>
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </Suspense>
+    <div className={classNames('app', {}, [theme])}>
+      <Suspense>
+        <Routes>
+          <Route path={Paths.root} element={<HomePage />}/>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </div>
   );
 };
 
