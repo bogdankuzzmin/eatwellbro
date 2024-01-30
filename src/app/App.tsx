66aae7e63de531b1953399c7b9e7c415b8d39b3c
bogdan <bogdankuzzmin@gmail.com>
2024-01-31 01:13:55 +0500

    restructure page folder

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,22 @@
 import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-const HomePage = lazy(() => import('@pages/Home'));
-const NotFoundPage = lazy(() => import('@pages/NotFound'));
+const MainPage = lazy(() => import('@pages/MainPage/ui/MainPage'));
+const NotFoundPage = lazy(() => import('@pages/NotFoundPage/ui/NotFoundPage'));
 
 import { useTheme } from '@app/providers/ThemeProvider';
 import { Paths } from '@utils/constants/routes';
 import classNames from '@utils/classNames';
 import '@app/styles/index.scss';
 
-const MainPage = () => {
+const App = () => {
   const {theme} = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
       <Suspense>
         <Routes>
-          <Route path={Paths.root} element={<HomePage />}/>
+          <Route path={Paths.root} element={<MainPage />}/>
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Suspense>
@@ -24,4 +24,4 @@ const MainPage = () => {
   );
 };
 
-export default MainPage;
+export default App;
