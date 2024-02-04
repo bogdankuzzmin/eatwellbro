b8ba2eaaedb3ba91ba11eb70d389a0793efc158d
bogdan <bogdankuzzmin@gmail.com>
2024-02-05 02:27:28 +0500

    add img extensions to global.d.ts

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -1,3 +1,13 @@
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.webp';
+declare module '*.svg' {
+  import React from "react";
+  const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
+  export default SVG;
+}
+
 declare module '*.scss' {
   interface IClassNames {
     [className: string]: string
