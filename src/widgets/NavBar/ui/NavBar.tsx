6ead3631b98400738bd90a13eabac52e49fc42cb
bogdan <bogdankuzzmin@gmail.com>
2024-02-10 04:47:49 +0500

    update navbar

--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -3,23 +3,30 @@ import { RoutePath } from '@shared/config/routeConfig';
 import Link from '@shared/ui/Link/Link';
 import classNames from '@shared/lib/classNames';
 import classes from './NavBar.module.scss';
+import ThemeSwitcher from '@shared/ui/ThemeSwitcher';
 
 interface INavBarProps {
   className?: string;
 }
 
 const NavBar = ({className}: INavBarProps) => (
-  <nav className={classNames(classes.Navbar, {}, [className])}>
-    <ul className={classes.List}>
-      <li className={classes.ListItem}>
-        <Link to={RoutePath.main}>Main</Link>
-      </li>
+  <div className={classNames(classes.Wrapper, {}, [className])}>
+    <nav className={classNames(classes.Navbar, {}, [className])}>
+      <span className={classes.Logo}>LOGO</span>
 
-      <li className={classes.ListItem}>
-        <Link to={RoutePath.about}>About</Link>
-      </li>
-    </ul>
-  </nav>
+      <ul className={classes.List}>
+        <li className={classes.ListItem}>
+          <Link to={RoutePath.main}>Main</Link>
+        </li>
+
+        <li className={classes.ListItem}>
+          <Link to={RoutePath.about}>About</Link>
+        </li>
+      </ul>
+    </nav>
+
+    <ThemeSwitcher/>
+  </div>
 );
 
 export default NavBar;
