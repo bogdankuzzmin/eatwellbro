9db6d7b733424a3fbe41d7e2d3380810ee31e593
bogdan <bogdankuzzmin@gmail.com>
2024-02-21 03:13:13 +0500

    install identity-obj-proxy

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -90,6 +90,10 @@ const config: Config = {
     'node'
   ],
 
+  moduleNameMapper: {
+    '\\.s?css$': 'identity-obj-proxy',
+  },
+
   modulePaths: ['<rootDir>src'],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
