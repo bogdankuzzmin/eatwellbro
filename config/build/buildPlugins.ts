794e2bfb087eadfe3dab6005eb8615f2ffc42bd8
bogdan <bogdankuzzmin@gmail.com>
2024-02-21 02:07:11 +0500

    install webpack bundle analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,7 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { HotModuleReplacementPlugin, ProgressPlugin,WebpackPluginInstance } from 'webpack';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 import { BuildOptions } from './types/config';
 
@@ -14,4 +15,7 @@ export const buildPlugins = ({ paths }: BuildOptions): WebpackPluginInstance[] =
     filename: 'css/[name].[contenthash:8].css',
   }),
   new HotModuleReplacementPlugin(),
+  new BundleAnalyzerPlugin({
+    openAnalyzer: false,
+  }),
 ]);
