ea3be60b272f7b9eb05ef51eb529b2f831b4f5ff
bogdan <bogdankuzzmin@gmail.com>
2024-02-23 23:50:15 +0500

    add aliases in jest config, add runtime: automatic in babel config

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -92,6 +92,13 @@ const config: Config = {
 
   moduleNameMapper: {
     '\\.s?css$': 'identity-obj-proxy',
+    '^@app(.*)$': '<rootDir>src/app$1',
+    '^@entities(.*)$': '<rootDir>src/entities$1',
+    '^@features(.*)$': '<rootDir>src/features$1',
+    '^@helpers(.*)$': '<rootDir>src/helpers$1',
+    '^@pages(.*)$': '<rootDir>src/pages$1',
+    '^@shared(.*)$': '<rootDir>src/shared$1',
+    '^@widgets(.*)$': '<rootDir>src/widgets$1',
   },
 
   modulePaths: ['<rootDir>src'],
